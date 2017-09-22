6696e4b36850741d57d718ad6613edf4c1671504
mosluce <mosluce@gmail.com>
2017-09-22 16:28:53 +0800

    init

---
+++ send.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curl -X POST -H "Authorization: key=YOUR-SERVER-KEY" -H "Content-Type: application/json" -d '{
+  "notification": {
+    "title": "Portugal vs. Denmark",
+    "body": "5 to 1",
+    "icon": "firebase-logo.png",
+    "click_action": "http://localhost:8081"
+  },
+  "to": "DEVICE_REGISTRATION_TOKEN"
+}' "https://fcm.googleapis.com/fcm/send"
